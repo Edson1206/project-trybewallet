ba59ddcb6bd181bf4b0e8a2ddc32c2236eed0887
Edson Pereira <ed.1206@gmail.com>
2022-08-29 00:47:17 -0300

    Resolvido requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,89 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+import { saveEmail } from '../redux/actions/index';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.buttonEnabled());
+  };
+
+  buttonEnabled = () => {
+    const { email, password } = this.state;
+    const validateEmail = /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/.test(email);
+    const five = 5;
+    if (password.length >= five
+        && validateEmail
+    ) {
+      this.setState({ disabled: false });
+    } else {
+      this.setState({ disabled: true });
+    }
+  };
+
+  handleClick = () => {
+    const { email } = this.state;
+    const { history, dispatch } = this.props;
+    dispatch(saveEmail(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login </div>;
+    const { email, password, disabled } = this.state;
+
+    return (
+      <div className="Login">
+        <h3 className="text-center">Login</h3>
+        <section className="login-inputs">
+          <input
+            type="email"
+            name="email"
+            onChange={ this.handleChange }
+            value={ email }
+            placeholder="E-mail"
+            data-testid="email-input"
+          />
+          <input
+            type="password"
+            name="password"
+            onChange={ this.handleChange }
+            value={ password }
+            placeholder="Senha"
+            data-testid="password-input"
+          />
+        </section>
+        <div className="button">
+          <button
+            data-testid="login-submit-button"
+            type="button"
+            disabled={ disabled }
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(Login);
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,20 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INICIAL_STATE = {
+  email: '',
+};
+
+const SAVE_EMAIL = 'SAVE_EMAIL';
+
+const userReducer = (state = INICIAL_STATE, action) => {
+  switch (action.type) {
+  case SAVE_EMAIL: {
+    return {
+      ...state,
+      email: action.email,
+    };
+  }
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
